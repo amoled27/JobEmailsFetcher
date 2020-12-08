556765552512ebf3fbb9d8ed3a79d90931dc8e61
amoled27 <amodshinde1995@gmail.com>
2020-12-08 20:07:49 +0530

    check flag

--- getmails.js
+++ getmails.js
@@ -1,3 +1,4 @@
+
 getEmails = {
     config: {
         filterText: 'jobs'
@@ -11,6 +12,7 @@ getEmails = {
     },
     execute: function () {
         this.getAllUnreadEmails();
+        //iterating through raw email data
         while (this.emailIndex < this.allUnreadEmailData.length) {
             let emailData = {
                 subject: this.getEmailSubject(),
@@ -20,29 +22,35 @@ getEmails = {
             this.emailIndex++;
         }
         this.addFilteredEmailsToStorage(this.filterEmails());
-        
     },
+    //get unreademails raw data
     getAllUnreadEmails: function () {
         this.allUnreadEmailData = document.querySelectorAll('.zA.zE');
     },
+    //filter the emails from the emails array as per string
     filterEmails: function () {
         let that = this;
         return this.unreadEmailsArr.filter(email => {
             if (email.subject.toLowerCase().indexOf(that.config.filterText) > -1) { return email };
         })
     },
+    //get email subject using query selector
     getEmailSubject: function () {
         return this.allUnreadEmailData[this.emailIndex].querySelector('.xY.a4W .xS .xT .y6 .bog .bqe').innerText;
     },
+    //get email sendername using query selector
     getSenderName: function () {
         return this.allUnreadEmailData[this.emailIndex].querySelector('.yX.xY .yW .bA4 .zF').innerText;
     },
+    //increment emails index to loop through the mails
     emailIndexIncrement() {
         return this.emailIndex++;
     },
+    //decrement emails index 
     emailIndexDecrement() {
         return this.emailIndex--;
     },
+    //add final filtered data to the chrome storage
     addFilteredEmailsToStorage(filteredEmails) {
         let that = this;
         chrome.storage.sync.set({ filteredEmails: filteredEmails }, function () {
@@ -50,8 +58,11 @@ getEmails = {
             that.onExecutionEnd();
         });
     },
+    //on execution end to set the execution flag to false
     onExecutionEnd() {
-        this.executionFlag = false;
+        chrome.storage.sync.set({ executionFlag: true }, function () {
+            console.log('flag set');
+        });
     }
 }
 
--- popup.js
+++ popup.js
@@ -1,22 +1,70 @@
 jobEmailsFilter = {
- init: function()  {
-    return this.execute();
- },
- execute: function() {
-    this.runEmailsScript();
- },
- runEmailsScript() {
-    return chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(tabs[0].id, { file: "getmails.js" });
-    });
- }
+   init: function () {
+      return this.execute();
+   },
+   execute: function () {
+      this.runEmailsScript();
+      // localStorage.setItem('executeFlag', 'pending');
+      executionFlag = true;
+      this.checkForEmailScriptCompletion();
+   },
+
+   //run script to get filtered mails from gmail
+   runEmailsScript() {
+      return chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+         chrome.tabs.executeScript(tabs[0].id, { file: "getmails.js" });
+      });
+   },
+
+   //get update button
+   getUpdateBtn: function () {
+      return document.getElementById('updateBtn');
+   },
+
+   //render the emails in chrome pop up
+   displayEmailsInPopUp: function (filteredEmailData) {
+      this.filteredEmailData.forEach(email => {
+         let emailElement = `<div class="email">
+                  <p id="sender">${email.name}</p>
+                  <p id="subject">${email.subject}</p>
+                  </div>`;
+         this.getPopUpElmentForRendring().innerHTML += emailElement + `</br>`;
+
+      });
+   },
+
+   //get element in HTML to render the list
+   getPopUpElmentForRendring: function () {
+      return document.getElementById('emailsList');
+   },
+
+   //get emails from storage
+   getEmailsFromStorage: function() {
+      let that = this;
+      chrome.storage.sync.get(['filteredEmails'], function (res) {
+         that.displayEmailsInPopUp(res.filteredEmails);
+     });
+   },
+
+   //check for execution completion of email script
+   checkForEmailScriptCompletion: function() {
+      // while(1){
+         if (this.checkExectionFlagInStorage()) {
+            this.getEmailsFromStorage();
+            break;
+         }
+      // }
+   },
+   checkExectionFlagInStorage: function() {
+      return chrome.storage.sync.get(['executionFlag'], function (res) {
+        return res.executionFlag;
+     });
+   }
 }
 
-const getUpdateBtn = function () {
-   return document.getElementById('updateBtn');
+//listent to onlick of Updatebtn
+jobEmailsFilter.getUpdateBtn().onclick = function () {
+   jobEmailsFilter.init();
 }
 
-getUpdateBtn().onclick = function (){
-    jobEmailsFilter.init();
-}
 
